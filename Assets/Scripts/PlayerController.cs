f38e8298af380bebe50a2676a781f6b42abaa3b6
Kamil <kamil.c1chy.krajewski@gmail.com>
2021-11-29 16:48:07 +0100

    Init

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+public Rigidbody2D rigidBody2D;
+
+    private void Update() {
+
+        if (Input.GetKey(KeyCode.A)) {
+            rigidBody2D.velocity = new Vector2(-5, 0);
+        }
+    }
+}
